1c72ab95845bfdc185ab45478a5f2374dc0ca06d
Stewart Hitchcock <Stewart.Hitchcock@gmail.com>
2020-05-26 16:36:18 +0100

    Header finished. Needs js. Contact page finished.

--- all-pages/header.php
+++ all-pages/header.php
@@ -5,7 +5,7 @@
             <a href="">My Services</a>
             <a href="">My Offices</a> 
             <a href="">Testimonials</a>
-            <a href="">Contact</a>
+            <a href="all-pages/contact.php">Contact</a>
             <a href=""><i class="fas fa-phone-alt"></i></a>
             <a href=""><i class="far fa-envelope"></i></a>
         </div>
