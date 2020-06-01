246f948ee7fbcd34b4f1fec424194a26951df3ed
Stewart Hitchcock <Stewart.Hitchcock@gmail.com>
2020-06-01 12:36:40 +0100

    Office changes

--- pages/offices-page/offices.php
+++ pages/offices-page/offices.php
@@ -8,14 +8,20 @@
     <script src="https://kit.fontawesome.com/ec34e9393d.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
+
+<?php $root = $_SERVER['DOCUMENT_ROOT']; ?>
     <?php include "$root/pages/header.php"; ?>
-    <div class = "office-cnt">
-        <div class = "">
-            <h2> </h2>
+    <?php include "header.php"; ?>
+    <div class = "office-bckgrnd">
+        <div class = "office-cnt">
+            <h2>My Offices</h2>
+            <div class = "office-spn"></div>
+            <h3>By Road<h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt tortor id velit dictum, id suscipit massa elementum. Curabitur efficitur purus nulla, et efficitur ex rhoncus vel. </p>
+            <h3>By Train<h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt tortor id velit dictum, id suscipit massa elementum. Curabitur efficitur purus nulla, et efficitur ex rhoncus vel. </p>
         </div>
     </div>
-    
     <?php include "$root/pages/footer.php"; ?>
 </body>
 </html>
