a426e071f5f20ab38892f93e4c61a01038ea1ec9
Adam Ferguson <61731043+AFerguson01@users.noreply.github.com>
2020-06-01 13:02:03 +0100

    Testimonials LInk Added

--- pages/landing-page/content.php
+++ pages/landing-page/content.php
@@ -48,7 +48,7 @@
         <a href="">Get In Touch</a>
     </div>
 </div>
-<div class = "testimonial-bckgrnd">
+<div class = "testimonial-bckgrnd" id="testimonials">
     <div class = "testimonial media">
         <i class="fas fa-quote-left"></i>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ut iaculis massa. Curabitur commodo velit ac aliquam tempor. Suspendisse dapibus dolor ut nibh ullamcorper, hendrerit aliquet ligula sollicitudin. Sed placerat accumsan libero ac ullamcorper. Proin pulvinar arcu in arcu tincidunt condimentum. Maecenas lorem mi, iaculis ut lacus laoreet, tempor dictum libero. Mauris faucibus eu velit id blandit. Integer vestibulum mi at est hendrerit, vel ornare ligula semper. Nulla congue porttitor massa, id convallis sapien dictum sed. Maecenas faucibus dictum ligula eu malesuada. Morbi placerat justo sit amet hendrerit aliquam. Donec pulvinar tortor sit amet velit ornare, at vehicula arcu suscipit. </p>
